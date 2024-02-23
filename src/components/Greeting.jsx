41f1ba1955e82abc2bfef775069b87f2398c427f
r.podobniy <r.podobniy@firstswgroup.com>
2024-02-23 16:39:52 +0300

    first commit

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,18 @@
+import { h } from 'preact';
+import { useState } from 'preact/hooks';
+
+export default function Greeting({ messages }) {
+	const randomMessage = () =>
+		messages[Math.floor(Math.random() * messages.length)];
+
+	const [greeting, setGreeting] = useState(messages[0]);
+
+	return (
+		<div>
+			<h3>{greeting}! Спасибо за ваш визит!</h3>
+			<button onClick={() => setGreeting(randomMessage())}>
+				Новое приветствие
+			</button>
+		</div>
+	);
+}
